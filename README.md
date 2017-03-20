e300b17726c9da38d44624d63be61687f101e015
Luiz cavalieri <luiz.cavalieri@gmail.com>
2017-03-20 23:47:35 +1100

    Email being sent via browser!

---
+++ README.md
@@ -0,0 +1,12 @@
+# HypotesisBe
+
+This project was generated with Express CLI.
+
+## Development server
+
+Run `npm start` for a dev server. 
+
+The server's port should be open `http://localhost:4100/`. 
+
+And the End point to send emails `http://localhost:4100/api/send-email`.
+
--- app.js
+++ app.js
@@ -13,19 +13,19 @@ var api = require('./routes/api');
 var app = express();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-var server = app.listen(4100, function () {
-
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-
-});
+//
+// var server = app.listen(3000, function () {
+//
+//     var host = server.address().address;
+//     var port = server.address().port;
+//
+//     console.log('Example app listening at http://%s:%s', host, port);
+//
+// });
 
 
 
